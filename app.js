17001951c6ce407ce956ad184740380d54bf4f39
Akanksha Verma <22906.akanksha@gmail.com>
2023-04-20 01:00:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const { myVariable } = require('./app.js');
-const socket = new WebSocket(`ws://localhost:${myVariable}/api`);
+
+const socket = new WebSocket(`ws://task-manager-terminal-app-production.up.railway.app/api`);
 const terminal = document.getElementById("terminal");
 const input = document.getElementById("input");
 const clearBtn = document.getElementById("clear");

ceee1c1821dd0beb2fb5845ee81858c5ea7327ce
Akanksha Verma <22906.akanksha@gmail.com>
2023-04-20 00:13:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -172,6 +172,6 @@ const server = http.createServer((req, res) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("Server listening on port 3000");
+server.listen(process.env.PORT, () => {
+  console.log(`Server listening on port `+process.env.PORT);
 });

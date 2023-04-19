caa16151f619a90f6cc7daa2d7845541e267d878
Akanksha Verma <22906.akanksha@gmail.com>
2023-04-20 00:07:03 +0530

    update

--- index.js
+++ index.js
@@ -1,303 +1,177 @@
-const Blink = "\x1b[5m%s\x1b[5m";
-const FgRed = "\x1b[31m%s\x1b[0m";
-const FgGreen = "\x1b[32m%s\x1b[0m";
-const FgYellow = "\x1b[33m%s\x1b[0m";
-const FgBlue = "\x1b[34m%s\x1b[0m";
-const FgMagenta = "\x1b[35m%s\x1b[0m";
-const FgCyan = "\x1b[36m%s\x1b[0m";
-const FgWhite = "\x1b[37m%s\x1b[0m";
-const BgRed = "\x1b[41m%s\x1b[0m";
-
+const http = require("http");
 const fs = require("fs");
+const WebSocket = require("ws");
 
-const args = process.argv;
+const server = http.createServer((req, res) => {
+  const filePath = `.${req.url}`;
 
-// The "index.js" is 8 characters long so -8
-// removes last 8 characters
-const currentWorkingDirectory = args[1].slice(0, -8);
-
-if (fs.existsSync(currentWorkingDirectory + "todo.txt") === false) {
-  let createStream = fs.createWriteStream("todo.txt");
-  createStream.end();
-}
-if (fs.existsSync(currentWorkingDirectory + "done.txt") === false) {
-  let createStream = fs.createWriteStream("done.txt");
-  createStream.end();
-}
-
-const InfoFunction = () => {
-  const UsageText = `
-Usage :-
-$ node index.js add "todo item"     # Add a new todo
-$ node index.js ls		    # Show remaining todos
-$ node index.js del NUMBER	     # Delete a todo
-$ node index.js done NUMBER	     # Complete a todo
-$ node index.js help		    # Show usage
-$ node index.js report		     # Statistics`;
-
-  console.log(FgMagenta, UsageText);
-};
-
-const listFunction = () => {
-  // Create a empty array
-  let data = [];
-
-  // Read from todo.txt and convert it into a string
-  const fileData = fs
-    .readFileSync(currentWorkingDirectory + "todo.txt")
-    .toString();
-
-  // Split the string and store into array
-  data = fileData.split("\n");
-
-  // Filter the string for any empty lines in the file
-  let filterData = data.filter(function (value) {
-    return value !== "";
-  });
-
-  if (filterData.length === 0) {
-    console.log(FgBlue, "There are no pending todos!");
-  }
-  for (let i = 0; i < filterData.length; i++) {
-    console.log(FgCyan, filterData.length - i + ". " + filterData[i]);
-  }
-};
-
-const addFunction = () => {
-  // New todo string argument is stored
-  const newTask = args[3];
-
-  // If argument is passed
-  if (newTask) {
-    // create a empty array
-    let data = [];
-
-    // Read the data from file todo.txt and
-    // convert it in string
-    const fileData = fs
-      .readFileSync(currentWorkingDirectory + "todo.txt")
-      .toString();
-
-    // New task is added to previous data
-    fs.writeFile(
-      currentWorkingDirectory + "todo.txt",
-      newTask + "\n" + fileData,
-
-      function (err) {
-        // Handle if there is any error
-        if (err) throw err;
-
-        // Logs the new task added
-        console.log(FgGreen, 'Added todo: "' + newTask + '"');
+  if (req.url === "/") {
+    // Serve the index page
+    fs.readFile("index.html", (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end(`An error occurred: ${err.message}`);
+        return;
       }
-    );
-  } else {
-    // no passed
-    console.log(BgRed, "Error: Missing todo string." + " Nothing added!");
-  }
-};
 
-const deleteFunction = () => {
-  // Store which index is passed
-  const deleteIndex = args[3];
-
-  // If index is passed
-  if (deleteIndex) {
-    // Create a empty array
-    let data = [];
-
-    // Read the data from file and convert
-    // it into string
-    const fileData = fs
-      .readFileSync(currentWorkingDirectory + "todo.txt")
-      .toString();
-
-    data = fileData.split("\n");
-    let filterData = data.filter(function (value) {
-      // Filter the data for any empty lines
-      return value !== "";
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
     });
 
-    // If delete index is greater than no. of task
-    // or less than zero
-    if (deleteIndex > filterData.length || deleteIndex <= 0) {
-      console.log(
-        BgRed,
-        "Error: todo #" + deleteIndex + " does not exist. Nothing deleted."
-      );
-    } else {
-      // Remove the task
-      filterData.splice(filterData.length - deleteIndex, 1);
-
-      // Join the array to form a string
-      const newData = filterData.join("\n");
-
-      // Write the new data back in file
-      fs.writeFile(
-        currentWorkingDirectory + "todo.txt",
-        newData,
-        function (err) {
-          if (err) throw err;
-
-          // Logs the deleted index
-          console.log(FgRed, "Deleted todo #" + deleteIndex);
-        }
-      );
-    }
-  } else {
-    // Index argument was no passed
-    console.log(BgRed, "Error: Missing NUMBER for deleting todo.");
+    return;
   }
-};
 
-const doneFunction = () => {
-  // Store the index passed as argument
-  const doneIndex = args[3];
+  if (req.url === "/api") {
+    // Handle WebSocket connections
+    const wss = new WebSocket.Server({ noServer: true });
 
-  // If argument is passed
-  if (doneIndex) {
-    // Empty array
-    let data = [];
+    wss.on("connection", (socket, req) => {
+      socket.on("message", (message) => {
+        const str = message.toString();
+        const [command, ...args] = str.split(" ");
 
-    // Create a new date object
-    let dateobj = new Date();
+        switch (command) {
+          case "list":
+            fs.readFile("data.txt", "utf-8", (err, data) => {
+              if (err) {
+                socket.send(
+                  `An error occurred while reading the file: ${err.message}`
+                );
+                return;
+              }
+              const entries = data.trim().split("\n");
+              if (entries.length === 0) {
+                socket.send("No entries found");
+                return;
+              }
+              const formattedEntries = entries
+                .map((entry) => entry.split(":").join(" - "))
+                .join("\n");
+              socket.send(formattedEntries);
+            });
+            break;
+          case "get":
+            const keyToGet = args[0];
+            fs.readFile("data.txt", "utf-8", (err, data) => {
+              if (err) {
+                socket.send(
+                  `An error occurred while reading the file: ${err.message}`
+                );
+                return;
+              }
+              const entries = data.trim().split("\n");
+              const matchingEntry = entries.find(
+                (entry) => entry.split(":")[0] === keyToGet
+              );
+              if (!matchingEntry) {
+                socket.send(`No matching entry found for key ${keyToGet}`);
+                return;
+              }
+              socket.send(matchingEntry);
+            });
+            break;
+          case "add":
+            const arg = args.join(" ");
+            const [keyToAdd, ...entry] = arg.split(" ");
+            //const [keyToAdd, ...valueToAdd] = args.join(" ").split(":");
+            const entryToAdd = `${keyToAdd}:${entry.join(" ")}\n`;
+            fs.appendFile("data.txt", entryToAdd, (err) => {
+              if (err) {
+                socket.send(
+                  `An error occurred while adding the entry: ${err.message}`
+                );
+                return;
+              }
+              socket.send(`Entry added: ${entryToAdd.trim()}`);
+            });
+            break;
+          case "update":
+            const [keyToUpdate, newValue] = args;
+            fs.readFile("data.txt", "utf-8", (err, data) => {
+              if (err) {
+                socket.send(
+                  `An error occurred while reading the file: ${err.message}`
+                );
+                return;
+              }
+              const entries = data.trim().split("\n");
+              const matchingIndex = entries.findIndex(
+                (entry) => entry.split(":")[0] === keyToUpdate
+              );
+              if (matchingIndex === -1) {
+                socket.send(`No matching entry found for key ${keyToUpdate}`);
+                return;
+              }
+              entries[matchingIndex] = `${keyToUpdate}:${newValue}`;
+              fs.writeFile("data.txt", entries.join("\n") + "\n", (err) => {
+                if (err) {
+                  socket.send(
+                    `An error occurred while updating the entry: ${err.message}`
+                  );
+                  return;
+                }
+                socket.send(`Entry updated: ${keyToUpdate}:${newValue}`);
+              });
+            });
 
-    // Convert it to string and slice only the
-    // date part, removing the time part
-    let dateString = dateobj.toISOString().substring(0, 10);
-
-    // Read the data from todo.txt
-    const fileData = fs
-      .readFileSync(currentWorkingDirectory + "todo.txt")
-      .toString();
-
-    // Read the data from done.txt
-    const doneData = fs
-      .readFileSync(currentWorkingDirectory + "done.txt")
-      .toString();
-
-    // Split the todo.txt data
-    data = fileData.split("\n");
-
-    // Filter for any empty lines
-    let filterData = data.filter(function (value) {
-      return value !== "";
+            break;
+          case "delete":
+            const keyToDelete = args[0];
+            fs.readFile("data.txt", "utf-8", (err, data) => {
+              if (err) {
+                socket.send(
+                  `An error occurred while reading the file: ${err.message}`
+                );
+                return;
+              }
+              const entries = data.trim().split("\n");
+              const matchingIndex = entries.findIndex(
+                (entry) => entry.split(":")[0] === keyToDelete
+              );
+              if (matchingIndex === -1) {
+                socket.send(`No matching entry found for key ${keyToDelete}`);
+                return;
+              }
+              entries.splice(matchingIndex, 1);
+              fs.writeFile("data.txt", entries.join("\n") + "\n", (err) => {
+                if (err) {
+                  socket.send(
+                    `An error occurred while deleting the entry: ${err.message}`
+                  );
+                  return;
+                }
+                socket.send(`Entry deleted: ${keyToDelete}`);
+              });
+            });
+            break;
+          default:
+            socket.send(`Invalid command: ${command}`);
+            break;
+        }
+      });
     });
 
-    // If done index is greater than
-    // no. of task or <=0
-    if (doneIndex > filterData.length || doneIndex <= 0) {
-      console.log(BgRed, "Error: todo #" + doneIndex + " does not exist.");
-    } else {
-      // Delete the task from todo.txt data
-      // and store it
-      const deleted = filterData.splice(filterData.length - doneIndex, 1);
+    // Upgrade the connection to a WebSocket connection
+    wss.handleUpgrade(req, req.socket, Buffer.alloc(0), (socket) => {
+      wss.emit("connection", socket, req);
+    });
 
-      // Join the array to create a string
-      const newData = filterData.join("\n");
+    return;
+  }
 
-      // Write back the data in todo.txt
-      fs.writeFile(
-        currentWorkingDirectory + "todo.txt",
-        newData,
-
-        function (err) {
-          if (err) throw err;
-        }
-      );
-      fs.writeFile(
-        // Write the stored task in done.txt
-        // along with date string
-        currentWorkingDirectory + "done.txt",
-        "x " + dateString + " " + deleted + "\n" + doneData,
-        function (err) {
-          if (err) throw err;
-          console.log(FgYellow, "Marked todo #" + doneIndex + " as done.");
-        }
-      );
+  // Serve static files
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.end("File Not Found");
+      return;
     }
-  } else {
-    // If argument was not passed
-    console.log(BgRed, "Error: Missing NUMBER for " + "marking todo as done.");
-  }
-};
 
-const reportFunction = () => {
-  // Create empty array for data of todo.txt
-  let todoData = [];
-
-  // Create empty array for data of done.txt
-  let doneData = [];
-
-  // Create a new date object
-  let dateobj = new Date();
-
-  // Slice the date part
-  let dateString = dateobj.toISOString().substring(0, 10);
-
-  // Read data from both the files
-  const todo = fs.readFileSync(currentWorkingDirectory + "todo.txt").toString();
-
-  const done = fs.readFileSync(currentWorkingDirectory + "done.txt").toString();
-
-  // Split the data from both files
-  todoData = todo.split("\n");
-
-  doneData = done.split("\n");
-  let filterTodoData = todoData.filter(function (value) {
-    return value !== "";
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end(data);
   });
-  let filterDoneData = doneData.filter(function (value) {
-    return value !== "";
-    // Filter both the data for empty lines
-  });
-  console.log(
-    FgMagenta,
-    dateString +
-      " " +
-      "Pending : " +
-      filterTodoData.length +
-      " Completed : " +
-      filterDoneData.length
-    // Log the stats calculated
-  );
-};
+});
 
-switch (args[2]) {
-  case "add": {
-    addFunction();
-    break;
-  }
-
-  case "ls": {
-    listFunction();
-    break;
-  }
-
-  case "del": {
-    deleteFunction();
-    break;
-  }
-
-  case "done": {
-    doneFunction();
-    break;
-  }
-
-  case "help": {
-    InfoFunction();
-    break;
-  }
-
-  case "report": {
-    reportFunction();
-    break;
-  }
-
-  default: {
-    InfoFunction();
-    // display help when no
-    // argument is passed or invalid
-    // argument is passed
-  }
-}
+server.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
